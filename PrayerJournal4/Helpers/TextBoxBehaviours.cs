4655e131be06cbc19a61d2d48078af4b900a4cf3
John van Dijk <fortyfour-three@teksavvy.com>
2024-07-09 10:05:43 -0400

    Select headline when entering box.

---
+++ PrayerJournal4/Helpers/TextBoxBehaviours.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PrayerJournal4.Helpers
+{
+    public static class TextBoxBehaviours
+    {
+        public static readonly DependencyProperty SelectAllOnFocusProperty =
+            DependencyProperty.RegisterAttached(
+                "SelectAllOnFocus",
+                typeof(bool),
+                typeof(TextBoxBehaviours),
+                new PropertyMetadata(false, OnSelectAllOnFocusChanged));
+
+        public static bool GetSelectAllOnFocus(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(SelectAllOnFocusProperty);
+        }
+
+        public static void SetSelectAllOnFocus(DependencyObject obj, bool value)
+        {
+            obj.SetValue(SelectAllOnFocusProperty, value);
+        }
+
+        private static void OnSelectAllOnFocusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox textBox)
+            {
+                if ((bool)e.NewValue)
+                {
+                    textBox.GotFocus += TextBox_GotFocus;
+                    textBox.PreviewMouseLeftButtonDown += TextBox_PreviewMouseLeftButtonDown;
+                }
+                else
+                {
+                    textBox.GotFocus -= TextBox_GotFocus;
+                    textBox.PreviewMouseLeftButtonDown -= TextBox_PreviewMouseLeftButtonDown;
+                }
+            }
+        }
+
+        private static void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                textBox.SelectAll();
+            }
+        }
+
+        private static void TextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TextBox textBox && !textBox.IsKeyboardFocusWithin)
+            {
+                e.Handled = true;
+                textBox.Focus();
+            }
+        }
+    }
+}
